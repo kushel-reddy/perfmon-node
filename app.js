7dee44336f00b56c124eb4b1ad923b12f73dc46a
kushel-reddy <kushel1422@gmail.com>
2024-06-13 23:11:09 +0530

    feat: -add socketio connection to stream the live performace metrics | -enhance fetch metrics api logic

--- app.js
+++ app.js
@@ -1,35 +1,123 @@
-const express = require('express');
-const app = express();
-const port = 3000;
 const { createClient } = require('@clickhouse/client');
+const express = require('express');
+const cors = require('cors');
+const http = require('http');
+const { Server } = require('socket.io');
+var moment = require('moment');
+
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true
+  }
+});
+const port = 3000;
 
 const clickhouse = createClient({
-  url: 'http://localhost:8123',
-  username: 'default', 
-  password: '', 
-  database: 'default', 
+  // url: 'http://localhost:8123',
+  url: 'http://ec2-13-233-250-35.ap-south-1.compute.amazonaws.com:8123',
+  username: 'default',
+  password: '',
+  database: 'default',
 });
 
+app.use(cors({
+  origin: 'http://localhost:3001'
+}));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.get('/query', async (req, res) => {
+const fetchMetricsSince = async (startTime) => {
+  const query = `
+      SELECT 
+          toUnixTimestamp(timestamp) AS interval,
+          CPU, system, usr, wait, guest, Command
+      FROM 
+          pidstat_data 
+      WHERE 
+          toUnixTimestamp(timestamp) > ${startTime} 
+      ORDER BY toUnixTimestamp(timestamp) ASC
+  `;
+  const rows = await clickhouse.query({
+    query: query,
+    format: 'JSONEachRow',
+  });
+  const dataset = await rows.json();
+  return dataset;
+};
+
+
+io.on('connection', (socket) => {
+  console.log('New client connected');
+  let startTime = moment().unix();
+
+  const fetchInitialMetrics = async () => {
+    const initialMetrics = await fetchMetricsSince(startTime);
+    socket.emit('metrics', initialMetrics);
+  };
+
+  fetchInitialMetrics();
+
+  const intervalId = setInterval(async () => {
+    const newMetrics = await fetchMetricsSince(startTime);
+    if (newMetrics.length > 0) {
+      socket.emit('metrics', newMetrics);
+      startTime = moment(newMetrics[newMetrics.length - 1].timestamp).unix();
+    }
+  }, 1000);
+
+  socket.on('disconnect', () => {
+    clearInterval(intervalId);
+    console.log('Client disconnected');
+  });
+});
+
+
+app.get('/metrics', async (req, res) => {
+  const { start, end, metric } = req.query;
+  if (!start || !end) {
+    return res.status(400).send('Missing start or end query parameters.');
+  }
+
+  const validMetrics = ['usr', 'system', 'guest', 'wait', 'CPU'];
+  if (!metric || !validMetrics.includes(metric)) {
+    return res.status(400).send('Invalid or missing metric query parameter.');
+  }
+
+  const startTimestamp = Math.floor(new Date(start).getTime() / 1000);
+  const endTimestamp = Math.floor(new Date(end).getTime() / 1000);
+
+  const query = `
+  SELECT 
+    Command,
+    toStartOfMinute(timestamp) AS interval,
+    avg(${metric}) AS ${metric}
+  FROM pidstat_data
+  WHERE toUnixTimestamp(timestamp) >= ${startTimestamp} 
+    AND toUnixTimestamp(timestamp) <= ${endTimestamp}
+  GROUP BY Command, toStartOfMinute(timestamp)
+  ORDER BY Command, toStartOfMinute(timestamp)
+  `;
+
   try {
-    const query = 'SELECT * FROM pidstat_data LIMIT 10'; 
-    const result = await clickhouse.query({
+    const rows = await clickhouse.query({
       query: query,
       format: 'JSONEachRow',
-    })
-    const dataset = await result.json()
+    });
+    const dataset = await rows.json()
     res.json(dataset);
   } catch (error) {
-    console.error('Error querying ClickHouse:', error);
-    res.status(500).send('Error querying ClickHouse');
+    res.status(500).send('Error querying ClickHouse: ' + error.message);
   }
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
--- perfmon-script.js
+++ perfmon-script.js
@@ -3,24 +3,23 @@ const { ClickHouse } = require('clickhouse');
 const moment = require('moment');
 
 const clickhouse = new ClickHouse({
-    url: 'http://172.31.43.109:8143',
+    url: 'http://172.31.43.109:8123',
     basicAuth: {
         username: 'default',
         password: '',
     },
-    isUseGzip: false,
+    isUseGzip: true,
     format: "json",
     config: {
         session_timeout: 60,
         output_format_json_quote_64bit_integers: 0,
-        enable_http_compression: 0
+        enable_http_compression: 1
     },
 });
 
-
 const createTableQuery = `
 CREATE TABLE IF NOT EXISTS pidstat_data (
-    datetime DateTime,
+    timestamp DateTime,
     UID String,
     PID String,
     usr Float64,
@@ -30,7 +29,7 @@ CREATE TABLE IF NOT EXISTS pidstat_data (
     CPU Float64,
     Command String
 ) ENGINE = MergeTree()
-ORDER BY datetime;
+ORDER BY (timestamp, PID);
 `;
 
 clickhouse.query(createTableQuery).toPromise().then(() => {
@@ -38,7 +37,6 @@ clickhouse.query(createTableQuery).toPromise().then(() => {
 }).catch(err => {
     console.error('Error creating table:', err);
 });
-
 
 const runPidstat = () => {
     return new Promise((resolve, reject) => {
@@ -52,7 +50,6 @@ const runPidstat = () => {
     });
 };
 
-
 const parsePidstatOutput = (output) => {
     const lines = output.trim().split('\n');
     const data = [];
@@ -62,8 +59,8 @@ const parsePidstatOutput = (output) => {
     for (let i = startIndex; i < lines.length; i++) {
         const fields = lines[i].trim().split(/\s+/);
         if (fields.length > 0 && fields[1] !== 'UID' && fields[0] !== 'Average:' && fields[0] !== '') {
-            console.log(fields[0])
             const time = fields[0];
+            const timestamp = moment(`${currentDate} ${time}`, 'YYYY-MM-DD HH:mm:ss').format('YYYY-MM-DD HH:mm:ss');
             const uid = fields[1];
             const pid = fields[2];
             const usr = parseFloat(fields[3]);
@@ -71,10 +68,10 @@ const parsePidstatOutput = (output) => {
             const guest = parseFloat(fields[5]);
             const wait = parseFloat(fields[6]);
             const cpu = parseFloat(fields[7]);
-            const command = fields[8];
+            const command = fields[9]
 
             data.push({
-                datetime: `${currentDate}`,
+                timestamp: timestamp,
                 UID: uid,
                 PID: pid,
                 usr: usr,
@@ -89,16 +86,17 @@ const parsePidstatOutput = (output) => {
     return data;
 };
 
-// Function to insert data into ClickHouse
 const insertDataIntoClickhouse = async (data) => {
+    if (data.length === 0) return;
+
     try {
         console.log("Preparing to insert data into ClickHouse");
         const query = `
-        INSERT INTO pidstat_data (datetime, UID, PID, usr, system, guest, wait, CPU, Command) 
+        INSERT INTO pidstat_data (timestamp, UID, PID, usr, system, guest, wait, CPU, Command) 
         VALUES 
         `;
-        const values = data.map(entry => `(
-            '${entry.datetime}', 
+        const values = data.map(entry => `( 
+            '${entry.timestamp}', 
             '${entry.UID}', 
             '${entry.PID}', 
             ${entry.usr}, 
@@ -113,20 +111,24 @@ const insertDataIntoClickhouse = async (data) => {
         console.log("Data inserted successfully");
     } catch (e) {
         console.error(`An error occurred during data insertion: ${e}`);
-        throw e;
     }
 };
 
+const parseAndInsertPidstatOutput = async (output) => {
+    const parsedMetrics = parsePidstatOutput(output);
+    await insertDataIntoClickhouse(parsedMetrics);
+};
 
 const main = async () => {
-    try {
-        const pidstatOutput = await runPidstat();
-        console.log(pidstatOutput)
-        const parsedMetrics = parsePidstatOutput(pidstatOutput);
-        await insertDataIntoClickhouse(parsedMetrics);
-        console.log(parsedMetrics);
-    } catch (e) {
-        console.error(`Failed to insert data: ${e}`);
+    while (true) {
+        try {
+            const pidstatOutput = await runPidstat();
+            await parseAndInsertPidstatOutput(pidstatOutput);
+        } catch (e) {
+            console.error(`Failed to run or process pidstat: ${e}`);
+        }
+
+        await new Promise(resolve => setTimeout(resolve, 100));
     }
 };
 
